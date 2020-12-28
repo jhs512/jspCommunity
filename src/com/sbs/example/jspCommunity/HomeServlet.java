4a8423e6ac03e06d520fd847c928e25fd8be1075
jhs512 <jangka512@gmail.com>
2020-12-28 11:46:08 +0900

    각국의 언어로 인사하는 HomeServlet 구현

--- src/com/sbs/example/jspCommunity/HomeServlet.java
+++ src/com/sbs/example/jspCommunity/HomeServlet.java
@@ -1,6 +1,7 @@
 package com.sbs.example.jspCommunity;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +14,33 @@ import javax.servlet.http.HttpServletResponse;
 public class HomeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().append("HelloWorld");
+		// 입력된 데이터의 문자셋은 UTF-8 이다.
+		request.setCharacterEncoding("UTF-8");
+		// 출력될 문서는 html(UTF-8) 이다.
+		response.setContentType("text/html; charset=UTF-8");
+
+		String lang = request.getParameter("lang");
+		int count = request.getParameter("count") != null ? Integer.parseInt(request.getParameter("count")) : 1;
+
+		if (lang == null) {
+			lang = "영어";
+		}
+
+		response.getWriter().append("<h1>입력된 언어 : " + lang + "</h1>");
+		response.getWriter().append("<br>");
+
+		String hello = "";
+
+		if (lang.equals("한국어")) {
+			hello = "안녕하세요.";
+		} else if (lang.equals("일본어")) {
+			hello = "ゴンニチと";
+		} else {
+			hello = "Hello";
+		}
+
+		for (int i = 1; i <= count; i++) {
+			response.getWriter().append(hello + "<br>");
+		}
 	}
 }
