cccf2063eb446cfb3b24e12a5b9a74967e4a6628
jhs512 <jangka512@gmail.com>
2021-01-27 12:35:55 +0900

    하단 페이지 메뉴 구현

--- WebContent/jsp/usr/article/list.jsp
+++ WebContent/jsp/usr/article/list.jsp
@@ -35,7 +35,7 @@
 		DoSearchForm__submited = true;
 	}
 	</script>
-	<form action="" onsubmit="DoSearchForm__submit(this); return false;">
+	<form onsubmit="DoSearchForm__submit(this); return false;">
 		<input type="hidden" name="boardId" value="${param.boardId}" />
 		
 		<select name="searchKeywordType">
@@ -63,7 +63,6 @@
 
 <hr />
 
-
 <c:forEach items="${articles}" var="article">
 <div>
 	번호 : ${article.id}
@@ -79,4 +78,44 @@
 	<hr />
 </div>
 </c:forEach>
+
+<style>
+.red {
+	color:red;
+}
+</style>
+
+<div class="con">
+	<!--
+	<c:set var="aUrl" value="?page=1&boardId=${param.boardId}&searchKeywordType=${param.searchKeywordType}&searchKeyword=${param.searchKeyword}" />
+	<a href="${aUrl}">◀◀</a>
+	-->
+
+	<c:if test="${pageBoxStartBeforeBtnNeedToShow}">
+		<c:set var="aUrl" value="?page=${pageBoxStartBeforePage}&boardId=${param.boardId}&searchKeywordType=${param.searchKeywordType}&searchKeyword=${param.searchKeyword}" />
+		<a href="${aUrl}">◀</a>
+	</c:if>
+	<c:forEach var="i" begin="${pageBoxStartPage}" end="${pageBoxEndPage}" step="1">
+		<c:set var="aClass" value="${page == i ? 'red' : ''}" />
+		<c:set var="aUrl" value="?page=${i}&boardId=${param.boardId}&searchKeywordType=${param.searchKeywordType}&searchKeyword=${param.searchKeyword}" />
+		<a class="${aClass}" href="${aUrl}">${i}</a>
+	</c:forEach>
+	
+	<c:if test="${pageBoxEndAfterBtnNeedToShow}">
+		<c:set var="aUrl" value="?page=${pageBoxEndAfterPage}&boardId=${param.boardId}&searchKeywordType=${param.searchKeywordType}&searchKeyword=${param.searchKeyword}" />
+		<a href="${aUrl}">▶</a>
+	</c:if>
+	
+	<!--
+	<c:set var="aUrl" value="?page=${totalPage}&boardId=${param.boardId}&searchKeywordType=${param.searchKeywordType}&searchKeyword=${param.searchKeyword}" />
+	<a href="${aUrl}">▶▶</a>
+	-->
+</div>
+
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+
 <%@ include file="../../part/foot.jspf"%>
--- src/com/sbs/example/jspCommunity/controller/UsrArticleController.java
+++ src/com/sbs/example/jspCommunity/controller/UsrArticleController.java
@@ -23,11 +23,11 @@ public class UsrArticleController {
 	public String showList(HttpServletRequest req, HttpServletResponse resp) {
 		String searchKeyword = req.getParameter("searchKeyword");
 		String searchKeywordType = req.getParameter("searchKeywordType");
-		
+
 		int itemsInAPage = 30;
 		int page = Util.getAsInt(req.getParameter("page"), 1);
 		int limitStart = (page - 1) * itemsInAPage;
-		
+
 		int boardId = Integer.parseInt(req.getParameter("boardId"));
 
 		Board board = articleService.getBoardById(boardId);
@@ -36,8 +36,49 @@ public class UsrArticleController {
 		int totalCount = articleService.getArticlesCountByBoardId(boardId, searchKeywordType, searchKeyword);
 		List<Article> articles = articleService.getForPrintArticlesByBoardId(boardId, limitStart, itemsInAPage, searchKeywordType, searchKeyword);
 
+		int totalPage = (int) Math.ceil(totalCount / (double) itemsInAPage);
+		
+		int pageBoxSize = 10;
+
+		// 현재 페이지 박스 시작, 끝 계산
+		
+		int previousPageBoxesCount = (page - 1) / pageBoxSize;
+		int pageBoxStartPage = pageBoxSize * previousPageBoxesCount + 1;
+		int pageBoxEndPage = pageBoxStartPage + pageBoxSize - 1;
+
+		if (pageBoxEndPage > totalPage) {
+			pageBoxEndPage = totalPage;
+		}
+
+		// 이전버튼 페이지 계산
+		int pageBoxStartBeforePage = pageBoxStartPage - 1;
+		if (pageBoxStartBeforePage < 1) {
+			pageBoxStartBeforePage = 1;
+		}
+
+		// 다음버튼 페이지 계산
+		int pageBoxEndAfterPage = pageBoxEndPage + 1;
+
+		if (pageBoxEndAfterPage > totalPage) {
+			pageBoxEndAfterPage = totalPage;
+		}
+
+		// 이전버튼 노출여부 계산
+		boolean pageBoxStartBeforeBtnNeedToShow = pageBoxStartBeforePage != pageBoxStartPage;
+		// 다음버튼 노출여부 계산
+		boolean pageBoxEndAfterBtnNeedToShow = pageBoxEndAfterPage != pageBoxEndPage;
+
 		req.setAttribute("totalCount", totalCount);
 		req.setAttribute("articles", articles);
+		req.setAttribute("totalPage", totalPage);
+		req.setAttribute("page", page);
+		
+		req.setAttribute("pageBoxStartBeforeBtnNeedToShow", pageBoxStartBeforeBtnNeedToShow);
+		req.setAttribute("pageBoxEndAfterBtnNeedToShow", pageBoxEndAfterBtnNeedToShow);
+		req.setAttribute("pageBoxStartBeforePage", pageBoxStartBeforePage);
+		req.setAttribute("pageBoxEndAfterPage", pageBoxEndAfterPage);
+		req.setAttribute("pageBoxStartPage", pageBoxStartPage);
+		req.setAttribute("pageBoxEndPage", pageBoxEndPage);
 
 		return "usr/article/list";
 	}
