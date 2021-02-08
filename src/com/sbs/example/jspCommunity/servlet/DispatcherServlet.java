7b8e4a7ad719f62200b6953a19c668ecf55ff0ed
jhs512 <jangka512@gmail.com>
2021-02-08 13:31:39 +0900

    jsp 폴더 경로가 바뀌면서 발생한 오류 제거

--- src/com/sbs/example/jspCommunity/servlet/DispatcherServlet.java
+++ src/com/sbs/example/jspCommunity/servlet/DispatcherServlet.java
@@ -146,7 +146,7 @@ public abstract class DispatcherServlet extends HttpServlet {
 				req.setAttribute("alertMsg", "로그인 후 이용해주세요.");
 				req.setAttribute("replaceUrl", "../member/login?afterLoginUrl=" + encodedCurrentUrl);
 
-				RequestDispatcher rd = req.getRequestDispatcher("/jsp/common/redirect.jsp");
+				RequestDispatcher rd = req.getRequestDispatcher(getJspDirPath() + "/common/redirect.jsp");
 				rd.forward(req, resp);
 			}
 		}
@@ -170,7 +170,7 @@ public abstract class DispatcherServlet extends HttpServlet {
 				req.setAttribute("alertMsg", "로그아웃 후 이용해주세요.");
 				req.setAttribute("historyBack", true);
 
-				RequestDispatcher rd = req.getRequestDispatcher("/jsp/common/redirect.jsp");
+				RequestDispatcher rd = req.getRequestDispatcher(getJspDirPath() + "/common/redirect.jsp");
 				rd.forward(req, resp);
 			}
 		}
@@ -189,7 +189,11 @@ public abstract class DispatcherServlet extends HttpServlet {
 	private void doAfterAction(HttpServletRequest req, HttpServletResponse resp, String jspPath) throws ServletException, IOException {
 		MysqlUtil.closeConnection();
 
-		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/" + jspPath + ".jsp");
+		RequestDispatcher rd = req.getRequestDispatcher(getJspDirPath() + "/" + jspPath + ".jsp");
 		rd.forward(req, resp);
+	}
+	
+	private String getJspDirPath() {
+		return "/WEB-INF/jsp";
 	}
 }
